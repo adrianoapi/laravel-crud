249d8ca3f5f360e52b86474ff7e4c22d161c830a
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-19 18:41:00 -0200

    update na home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Home;
+use App\Blog;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view('home.index');
+        $blogs = Blog::all();
+        return view('home.index', ['blogs' => $blogs]);
     }
 
 }
